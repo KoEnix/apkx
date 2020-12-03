423e4724879f3b33153909686454eb06ecbb04f0
Koenix <j.u.2@web.de>
2020-12-03 09:59:36 -0500

    Updated to use latest version of enjarify

--- install.sh
+++ install.sh
@@ -3,7 +3,15 @@
 if [[ $EUID > 0 ]]; then
   echo "The installation script needs to be run as root."
   exit 1
+elif ! command -v git &> /dev/null; then
+  echo "git must be installed!"
+  exit 1
 else
-  cp apkx apkx-libs.jar enjarify.pex cfr.jar fernflower.jar /usr/local/bin
-  chmod 755 /usr/local/bin/apkx /usr/local/bin/enjarify.pex
+  mkdir -p /opt/apkx
+  cp apkx apkx-libs.jar cfr.jar fernflower.jar /opt/apkx
+  git clone https://github.com/Storyyeller/enjarify.git /opt/enjarify
+  chmod 755 /opt/apkx/apkx /opt/enjarify/enjarify.sh
+  ln -s "/opt/enjarify/enjarify.sh" /usr/local/bin/enjarify
+  ln -s "/opt/apkx/apkx" /usr/local/bin/apkx
+
 fi
